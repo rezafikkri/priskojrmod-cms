1c63c95dde6f1b008c3d17cc227d28e4e78eeb34
rezafikkri <fikkri.reza@gmail.com>
2025-07-08 09:21:39 +0700

    test: improve createSecretKey function test and add unit test for updateAppName function in secret-key-service

--- tests/lib/services/secret-key-service.test.js
+++ tests/lib/services/secret-key-service.test.js
@@ -8,6 +8,7 @@ import {
 } from 'vitest';
 import {
   createSecretKey,
+  updateAppName,
   deleteSecretKey,
   getSecretKeys,
   getSpecificSecretKey,
@@ -30,12 +31,17 @@ beforeAll(() => {
         findMany: vi.fn(),
         findUnique: vi.fn(),
         update: vi.fn(),
+        updateMany: vi.fn(),
       },
     },
   }));
 
-  vi.mock('uuid', () => ({
-    v7: () => 'uuidv7',
+  vi.mock('@/lib/pjme-prisma-client', () => ({
+    default: {
+      Product: {
+        findUnique: vi.fn(),
+      },
+    },
   }));
 });
 
@@ -64,23 +70,61 @@ describe('createSecretKey function', () => {
 
     const verifySession = (await import('@/lib/verifySession')).default;
     const pjmaDBPrismaClient = (await import('@/lib/pjma-prisma-client')).default;
+    const pjmeDBPrismaClient = (await import('@/lib/pjme-prisma-client')).default;
 
     verifySession.mockResolvedValue({ isAuth: true, userId: '123' });
     pjmaDBPrismaClient.SecretKeyLicense.create.mockResolvedValue({ id: 1n });
+    pjmeDBPrismaClient.Product.findUnique.mockResolvedValue({ name: 'Product Name' });
 
     await createSecretKey({
+      product_id: '24dd4d78-ead8-45b8-bfa5-e2bb289cb4d2',
       key: '8f23fcc4c918eb26c991b3950c79a243a6b0d683c2e58e0d31fc367b652e2b05',
-      app_name: 'test-app',
     });
 
     expect(pjmaDBPrismaClient.SecretKeyLicense.create).toHaveBeenCalledWith({
       data: {
         key: '8f23fcc4c918eb26c991b3950c79a243a6b0d683c2e58e0d31fc367b652e2b05',
-        app_name: 'test-app',
-        product_id: 'uuidv7',
+        app_name: 'Product Name',
+        product_id: '24dd4d78-ead8-45b8-bfa5-e2bb289cb4d2',
         created_at: BigInt(Math.floor(new Date().getTime() / 1000)),
       },
       select: { id: true },
+    });
+  });
+});
+
+describe('updateAppName function', () => {
+  it('Should call verifySession function, not call pjmaDBPrismaClient.SecretKeyLicense.update function and throw Error with "Unauthenticated" message', async () => {
+    const verifySession = (await import('@/lib/verifySession')).default;
+    const pjmaDBPrismaClient = (await import('@/lib/pjma-prisma-client')).default;
+
+    verifySession.mockResolvedValue(false);
+
+    await expect(
+      updateAppName({
+        product_id: '24dd4d78-ead8-45b8-bfa5-e2bb289cb4d2',
+        name: 'New Product Name',
+      }),
+    ).rejects.toThrow('Unauthenticated');
+
+    expect(verifySession).toHaveBeenCalled();
+    expect(pjmaDBPrismaClient.SecretKeyLicense.updateMany).not.toHaveBeenCalled();
+  });
+
+  it('Should call pjmaDBPrismaClient.SecretKeyLicense.update function correctly', async () => {
+    const verifySession = (await import('@/lib/verifySession')).default;
+    const pjmaDBPrismaClient = (await import('@/lib/pjma-prisma-client')).default;
+
+    verifySession.mockResolvedValue({ isAuth: true, userId: 'user-id' });
+
+    await updateAppName({
+      product_id: '24dd4d78-ead8-45b8-bfa5-e2bb289cb4d2',
+      name: 'Updated Name',
+    });
+
+    expect(pjmaDBPrismaClient.SecretKeyLicense.updateMany).toHaveBeenCalledWith({
+      where: { product_id: '24dd4d78-ead8-45b8-bfa5-e2bb289cb4d2' },
+      data: { app_name: 'Updated Name' },
     });
   });
 });
