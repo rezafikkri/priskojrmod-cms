b43b4c0463388f172f5c6a14fb135d1a85aa4723
rezafikkri <fikkri.reza@gmail.com>
2025-09-12 10:50:07 +0700

    feat: create seeder for transactions

--- constants/enums.js
+++ constants/enums.js
@@ -12,3 +12,16 @@ export const Language = Object.freeze({
   ID: 'id',
   EN: 'en',
 });
+
+export const TransactionStatus = Object.freeze({
+  PENDING: 'pending',
+  PAID: 'paid',
+  CANCELLED: 'cancelled',
+  REFUND: 'refund',
+});
+
+export const ShareMethod = Object.freeze({
+  DOWNLOAD_LINK: 'download_link',
+  DRIVE_SHARE: 'drive_share',
+  MANUAL_REQUIRED: 'manual_required',
+});
--- prisma/seed-dev-data.mjs
+++ prisma/seed-dev-data.mjs
@@ -3,9 +3,13 @@ import { faker } from '@faker-js/faker';
 import { PrismaClient as PjmaDBPrismaClient } from '../prisma-pjma-db/pjma-db-client/index.js';
 import { PrismaClient as PjmeDBPrismaClient } from '../prisma-pjme-db/pjme-db-client/index.js';
 import { v7 as uuidv7 } from 'uuid';
+import { PriceType, TransactionStatus } from '../constants/enums.js';
+import { generateDocumentCode } from '../lib/generate-document-code.js';
 
 const pjmaDBPrismaClient = new PjmaDBPrismaClient();
 const pjmeDBPrismaClient = new PjmeDBPrismaClient();
+
+export const generateRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 function generateLicenseKeyPayload(email, licenseKeyId) {
   const expiresAt = new Date();
@@ -23,11 +27,11 @@ function generateLicenseKeyCode(payload, secret) {
   return jwt.sign(payload, secret);
 }
 
-export default async function seedDevData() {
+async function seedCustomersAndLicenseKeys() {
   // seed customers
   const customers = [];
   const customerIds = [];
-  for (let i = 0; i < 70; i++) {
+  for (let i = 0; i < 0; i++) {
     const currentTime = BigInt(Math.floor((Date.now() / 1000) - (60 * 60 * 24 * i)));
     const customerId = uuidv7();
     customerIds.push(customerId);
@@ -63,7 +67,7 @@ export default async function seedDevData() {
   // seed license keys
   const licenseKeys = [];
 
-  for (let i = 0; i < 70; i++) {
+  for (let i = 0; i < 0; i++) {
     const email = faker.internet.email().toLowerCase();
     const currentTime = BigInt(Math.floor((Date.now() / 1000) - (60 * 60 * 24 * i)));
     const secret = await pjmaDBPrismaClient.secretKeyLicense.findFirst({
@@ -90,6 +94,31 @@ export default async function seedDevData() {
   });
 
   console.log(`✅ Seeded ${licenseKeys.length} license keys`);
+}
+
+function getTransactionDetails({
+  max,
+  products,
+}) {
+  const transactionDetails = [];
+  for (const product of products) {
+    transactionDetails.push({
+      product_id: product.id,
+      product_price_id: product.variants[0].prices[0].id,
+      quantity: generateRandomInt(2, 5),
+
+      product_name: product.name,
+      product_variant: product.variants[0].name,
+      product_currency_code: product.variants[0].prices[0].currency_code,
+      product_price: product.variants[0].prices[0].price.toNumber(),
+    });
+    if (transactionDetails.length === max) break;
+  }
+  return transactionDetails;
+}
+
+export default async function seedDevData() {
+  await seedCustomersAndLicenseKeys();
 
   // seed admin
   const admin = await pjmeDBPrismaClient.admin.findFirst({
@@ -110,4 +139,56 @@ export default async function seedDevData() {
     });
     console.log(`✅ Seeded admin fikkri.reza@gmail.com`);
   }
+
+  // seed transaction
+  const products = await pjmeDBPrismaClient.product.findMany({
+    where: { price_type: PriceType.PAID },
+    include: {
+      variants: {
+        include: {
+          prices: true,
+        },
+      },
+    },
+  });
+  const customers = await pjmeDBPrismaClient.customer.findMany();
+
+  const transactions = [];
+  for (let i = 0; i < 4; i++) {
+    let transactionDetails = [];
+    if (i % 2 === 0) {
+      transactionDetails = getTransactionDetails({ max: 3, products });
+    } else if (i % 3 === 0) {
+      transactionDetails = getTransactionDetails({ max: 2, products });
+    } else {
+      transactionDetails = getTransactionDetails({ max: 1, products });
+    }
+
+    const selectedCustomer = customers[generateRandomInt(0, customers.length - 1)];
+    const currentTime = BigInt(Math.floor((Date.now() / 1000) - (60 * 60 * 24 * i)));
+    transactions.push({
+      customer_id: selectedCustomer.id,
+      status: TransactionStatus.PENDING,
+      code: generateDocumentCode('TRX'),
+      currency_code: transactionDetails[0].product_currency_code,
+      total_amount: transactionDetails
+        .reduce((total, { quantity, product_price }) => total + (product_price * quantity), 0),
+      customer_name: selectedCustomer.first_name,
+      customer_email: selectedCustomer.email,
+      customer_phone_number: selectedCustomer.phone_number,
+      created_at: currentTime,
+      updated_at: currentTime,
+      details: {
+        create: transactionDetails,
+      },
+    });
+  }
+
+  for (let transaction of transactions) {
+    await pjmeDBPrismaClient.transaction.create({
+      data: transaction,
+    });
+  }
+
+  console.log(`✅ Seeded ${transactions.length} transactions`);
 }
