fa74779edb47da331464462db9e4774325496b1e
rezafikkri <fikkri.reza@gmail.com>
2025-12-29 07:25:26 +0700

    feat: implement error.jsx in root level for handle unexpected errors

---
+++ app/error.jsx
@@ -0,0 +1,36 @@
+'use client' // Error boundaries must be Client Components
+
+import { Montserrat } from 'next/font/google';
+import './globals.css';
+import { useEffect } from 'react';
+import { Button } from '@/components/ui/button';
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-montserrat',
+  fallback: ['sans-serif'],
+});
+
+export default function Error({ error, reset }) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error)
+  }, [error])
+
+  return (
+    <div className={`${montserrat.variable} w-full h-screen flex flex-col justify-center items-center`}>
+      <h2 className="text-xl font-medium mb-7">Something went wrong!</h2>
+      <Button
+        className="h-auto inline-block text-base px-3 py-1.5"
+        variant="secondary"
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </Button>
+    </div>
+  )
+}
