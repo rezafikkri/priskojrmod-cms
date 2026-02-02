6db26d01d030a0f1fa6b08325e8c54fe3a481daf
rezafikkri <fikkri.reza@gmail.com>
2026-02-02 20:18:11 +0700

    fix: fix error when product category in transaction there is application, then change/correct transaction status but one or all product in transaction has been deleted

--- prisma/seed.js
+++ prisma/seed.js
@@ -22,7 +22,7 @@ async function main() {
   if (process.env.NODE_ENV === 'development') {
     // const customers = await seedCustomers(prisma, 0);
     // await seedLicenseKeys(prisma, customers);
-    await seedTransactions(prisma, 2);
+    await seedTransactions(prisma, 3);
   }
 }
 
