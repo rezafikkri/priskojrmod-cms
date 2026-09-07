8c09ed71d4289925cb984b6721aa8958ee4ef713
rezafikkri <fikkri.reza@gmail.com>
2026-09-07 20:38:30 +0700

    refactor: add more description for revoke license-key dialog, regarding the potential customer impact related to the license key expiration

--- components/license-key/edit-revoke-status-dialog.jsx
+++ components/license-key/edit-revoke-status-dialog.jsx
@@ -22,13 +22,15 @@ export default function EditRevokeStatusDialog({
   }
 
   let title;
-  let description;
+  let descriptionP1;
+  let descriptionP2;
   if (editRevokeStatusData?.isRevoked) {
     title = 'Unrevoke License Key';
-    description = `License key owned by customer <b>${editRevokeStatusData?.name}</b> <span class="break-all">(${editRevokeStatusData?.email})</span> for app <b>${editRevokeStatusData?.appName}</b> will be <b>unrevoked</b>. As a result, this license key can be used again to activate and access the application.`;
+    descriptionP1 = `License key owned by customer <b>${editRevokeStatusData?.name}</b> <span class="break-all">(${editRevokeStatusData?.email})</span> for app <b>${editRevokeStatusData?.appName}</b> will be <b>unrevoked</b>. As a result, this license key can be used again to activate and access the application.`;
   } else {
     title = 'Revoke License Key';
-    description = `License key owned by customer <b>${editRevokeStatusData?.name}</b> <span class="break-all">(${editRevokeStatusData?.email})</span> for app <b>${editRevokeStatusData?.appName}</b> will be <b>revoked</b>. As a result, this license key can no longer be used to activate or access the application.`;
+    descriptionP1 = `License key owned by customer <b>${editRevokeStatusData?.name}</b> <span class="break-all">(${editRevokeStatusData?.email})</span> for app <b>${editRevokeStatusData?.appName}</b> will be <b>revoked</b>.`;
+    descriptionP2 = 'As a result, this license key can no longer be used to activate or access the application. <b>Make sure this is based on a valid reason</b>, as an incorrect revoke could harm the customer regarding their license key\'s expired at.';
   }
 
   function handleClickOutside(e) {
@@ -46,9 +48,15 @@ export default function EditRevokeStatusDialog({
         <DialogHeader className="text-left">
           <DialogTitle className="text-xl">{title}</DialogTitle>
           <DialogDescription
-            className="text-base my-1.5 text-zinc-700 dark:text-zinc-300 [&_b]:font-semibold" 
-            dangerouslySetInnerHTML={{ __html: description }}
+            className="text-base mt-1.5 text-zinc-700 dark:text-zinc-300 [&_b]:font-semibold" 
+            dangerouslySetInnerHTML={{ __html: descriptionP1 }}
           />
+          {descriptionP2 && (
+            <DialogDescription
+              className="text-base text-zinc-700 dark:text-zinc-300 [&_b]:font-semibold"
+              dangerouslySetInnerHTML={{ __html: descriptionP2 }}
+            />
+          )}
         </DialogHeader>
 
         <DialogFooter className="relative">
